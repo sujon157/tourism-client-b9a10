f08e51ab2be307078016fdf69141b7fb9036d7b3
sujon157 <riponahamed1579@gmail.com>
2024-08-29 19:44:59 +0600

    React ToolTip added

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import './Navbar.css';
+import { Tooltip } from 'react-tooltip'
 
 const Navbar = () => {
     const { user, logOut, loading } = useContext(AuthContext);
@@ -23,7 +24,7 @@ const Navbar = () => {
 
         } else {
             setTheme("light");
-       
+
         }
     };
 
@@ -41,18 +42,18 @@ const Navbar = () => {
     // const handleToggle = (e) => {
     //     if (e.target.checked) {
     //         setTheme('dark');
-            // localStorage.setItem('theme', 'dark');
+    // localStorage.setItem('theme', 'dark');
 
-            // document.querySelector('html').setAttribute('data-theme', 'dark');
-            // setTheme('dark');
+    // document.querySelector('html').setAttribute('data-theme', 'dark');
+    // setTheme('dark');
     //     }
     //     else {
 
     //         setTheme('light');
-            // localStorage.setItem('theme', 'light');
+    // localStorage.setItem('theme', 'light');
 
-            // document.querySelector('html').setAttribute('data-theme', 'light');
-            // setTheme('light');
+    // document.querySelector('html').setAttribute('data-theme', 'light');
+    // setTheme('light');
     //     }
     // }
 
@@ -109,7 +110,13 @@ const Navbar = () => {
                             {navLink}
                         </ul>
                     </div>
-                    <img src="https://i.ibb.co/w0d1F8J/airplane.png" className="w-14 h-14" alt="" />
+                    {/* <img src="https://i.ibb.co/w0d1F8J/airplane.png" className="w-14 h-14" alt="" /> */}
+                    <a className="my-anchor-elements">
+                        <img src="https://i.ibb.co/w0d1F8J/airplane.png" className="w-14 h-14" alt="" />
+                    </a>
+                    <Tooltip anchorSelect=".my-anchor-elements" place="top">
+                        Logo
+                    </Tooltip>
                     <a className="btn btn-ghost text-white font-bold text-xl">
 
                         <span className='text-3xl font-bold text-orange-600'>T</span>RAVEL TOUR</a>
@@ -120,48 +127,50 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end gap-2 p-6 ">
-                    {/* <input
-                        onChange={handleToggle}
-                        type="checkbox"
-                        className="toggle theme-controller col-span-2 col-start-1 row-start-1 border-sky-400 bg-amber-300 [--tglbg:theme(colors.sky.500)] checked:border-blue-800 checked:bg-blue-300 checked:[--tglbg:theme(colors.blue.900)]" /> */}
 
-                    <label className="cursor-pointer grid place-items-center pr-1 lg:pr-2">
-                        <input
-                            onChange={handleToggle}
-                            type="checkbox"
-                            checked={theme === "dark"}
-                            className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-                        />
-                        <svg
-                            className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="14"
-                            height="14"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <circle cx="12" cy="12" r="5" />
-                            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
-                        </svg>
-                        <svg
-                            className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="14"
-                            height="14"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        >
-                            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                        </svg>
-                    </label>
+
+                    <a data-tooltip-id="my-tooltip"
+                        data-tooltip-content="Theme"
+                        data-tooltip-place="top">
+                        <label className="cursor-pointer grid place-items-center pr-1 lg:pr-2">
+                            <input
+                                onChange={handleToggle}
+                                type="checkbox"
+                                checked={theme === "dark"}
+                                className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+                            />
+                            <svg
+                                className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="14"
+                                height="14"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <circle cx="12" cy="12" r="5" />
+                                <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                            </svg>
+                            <svg
+                                className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="14"
+                                height="14"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+                            </svg>
+                        </label>
+                    </a>
+                    <Tooltip id="my-tooltip" />
 
 
 
@@ -178,7 +187,14 @@ const Navbar = () => {
                             <>
                                 <div className="dropdown dropdown-bottom">
                                     <div tabIndex={0} role="button" className=" m-1 rounded-full w-10 h-10 mr-7 ">
-                                        <img src={user.photoURL} className="rounded-full w-10 h-10 mr-3" alt="" />
+
+                                        <a className="my-anchor-element"><img src={user.photoURL} className="rounded-full w-10 h-10 mr-3" alt="" /></a>
+
+                                        <Tooltip anchorSelect=".my-anchor-element" place="top">
+                                            Account
+                                        </Tooltip>
+
+
                                     </div>
                                     <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-24 p-2 shadow">
                                         <li><a onClick={handleSignOut}>SignOut</a></li>
